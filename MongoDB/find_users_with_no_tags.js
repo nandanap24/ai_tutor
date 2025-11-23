3c19dd0b4366e17fc5c69d16d74c6fdc63b237d0
NANDANA P <161133118+nandanap24@users.noreply.github.com>
2025-11-23 20:53:23 +0530

    Create find_users_with_no_tags.js

---
+++ MongoDB/find_users_with_no_tags.js
@@ -0,0 +1,15 @@
+db.users.find({tags: []})
+
+/*
+[
+  {
+    _id: ObjectId('691ffc9c101c4173ec9dc2ae'),
+    name: 'John',
+    age: 19,
+    course: 'CS',
+    score: 55,
+    city: 'Kozhikode',
+    tags: []
+  }
+]
+*/
