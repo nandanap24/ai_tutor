8583f661e84a50d68615230a48b26d0bff0e94fe
NANDANA P <161133118+nandanap24@users.noreply.github.com>
2025-11-24 11:27:27 +0530

    Update total_revenue_by_user.js

--- MongoDB/total_revenue_by_user.js
+++ MongoDB/total_revenue_by_user.js
@@ -10,3 +10,13 @@ db.orders.aggregate([{$group: {_id: "$user", totalRevenue: {$sum: {$multiply: ["
   { _id: 'Priya', totalRevenue: 15 }
 ]
 */
+/* QUESTION DATA
+  db.orders.insertMany([
+{ user: "Arya", item: "Apple", qty: 5, pricePer: 20, date: new Date("2025-11-01") },
+{ user: "Rohan", item: "Banana", qty: 10, pricePer: 5, date: new Date("2025-10-29") },
+{ user: "Meera", item: "Apple", qty: 2, pricePer: 20, date: new Date("2025-11-18") },
+{ user: "Sana", item: "Orange", qty: 4, pricePer: 15, date: new Date("2025-09-20") },
+{ user: "Rita", item: "Apple", qty: 12, pricePer: 20, date: new Date("2025-11-20") },
+{ user: "Priya", item: "Banana", qty: 3, pricePer: 5, date: new Date("2025-11-10") }
+])
+*/
