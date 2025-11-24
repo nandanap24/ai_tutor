dfe4ca927c12f2d61a9fe2eb875e2391c409a2f6
NANDANA P <161133118+nandanap24@users.noreply.github.com>
2025-11-24 11:28:46 +0530

    Update group_by_city.js

--- MongoDB/group_by_city.js
+++ MongoDB/group_by_city.js
@@ -7,3 +7,18 @@ db.users.aggregate([{$group: {_id: "$city", count: {$sum: 1}}}, {$match: {count:
   { count: 3, city: 'Kochi' }
 ]
  */
+/*
+  db.users.insertMany([
+{ name: "Arya", age: 21, course: "CS", score: 78, city: "Trivandrum", tags: ["beginner","web"] },
+{ name: "Rohan", age: 25, course: "CS", score: 92, city: "Kochi", tags: ["advanced","algorithms"]
+},
+{ name: "Neha", age: 22, course: "IT", score: 85, city: "Kozhikode", tags: ["web","design"] },
+{ name: "Akhil", age: 20, course: "ECE", score: 70, city: "Kottayam", tags: ["hardware"] },
+{ name: "Meera", age: 23, course: "IT", score: 88, city: "Kochi", tags: ["ml","web"] },
+{ name: "Sana", age: 27, course: "CS", score: 95, city: "Trivandrum", tags: ["ml","advanced"] },
+{ name: "Vikram", age: 24, course: "CS", score: 60, city: "Alleppey", tags: ["beginner"] },
+{ name: "Priya", age: 21, course: "IT", score: 82, city: "Kottayam", tags: ["web"] },
+{ name: "Rita", age: 29, course: "ECE", score: 91, city: "Kochi", tags: ["embedded","advanced"] },
+{ name: "John", age: 19, course: "CS", score: 55, city: "Kozhikode", tags: [] }
+])
+  */
