eccaf0d0df5e9fc3e16e83b3e6434823a1be1a6d
NANDANA P <161133118+nandanap24@users.noreply.github.com>
2025-11-25 11:15:02 +0530

    Create Employees_view_alldoc.js

---
+++ MongoDB/Employees_view_alldoc.js
@@ -0,0 +1,54 @@
+db.employees.find().pretty()
+
+/*
+[
+  {
+    _id: 1,
+    name: 'John Doe',
+    age: 28,
+    position: 'Software Engineer',
+    salary: 90000,
+    department: 'Engineering',
+    hire_date: ISODate('2021-01-15T00:00:00.000Z'),
+    bonus: 5000
+  },
+  {
+    _id: 2,
+    name: 'Jane Smith',
+    age: 34,
+    position: 'Project Manager',
+    salary: 95000,
+    department: 'Engineering',
+    hire_date: ISODate('2019-06-23T00:00:00.000Z'),
+    bonus: 5000
+  },
+  {
+    _id: 3,
+    name: 'Emily Johnson',
+    age: 41,
+    position: 'CTO',
+    salary: 150000,
+    department: 'Management',
+    hire_date: ISODate('2015-03-12T00:00:00.000Z')
+  },
+  {
+    _id: 4,
+    name: 'Michael Brown',
+    age: 29,
+    position: 'Software Engineer',
+    salary: 85000,
+    department: 'Engineering',
+    hire_date: ISODate('2020-07-30T00:00:00.000Z'),
+    bonus: 5000
+  },
+  {
+    _id: 5,
+    name: 'Sarah Davis',
+    age: 26,
+    position: 'UI/UX Designer',
+    salary: 70000,
+    department: 'Design',
+    hire_date: ISODate('2022-10-12T00:00:00.000Z')
+  }
+]
+*/
